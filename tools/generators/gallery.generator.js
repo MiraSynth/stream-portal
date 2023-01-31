ed30f9e716b5b311d33b32ca4d3ddc9b150c8725
Mira <97364991+MiraSynth@users.noreply.github.com>
2023-01-31 21:18:17 +0100

    add image gallery with some imaghes

--- tools/generators/gallery.generator.js
+++ tools/generators/gallery.generator.js
@@ -2,12 +2,12 @@ const { JSDOM } = require("jsdom");
 const { ExifImage } = require("exif");
 const sharp = require("sharp");
 const path = require("path")
-const { promises: { readFile, writeFile } } = require("fs");
+const { promises: { readFile, writeFile }, existsSync } = require("fs");
 
 const { findByType } = require("../utils/file-search.util")
 
 async function start() {
-    const target = "./photographs/index.html";
+    const target = "./gallery/index.html";
     const images = (await findByType("./assets/images/gallery", ".jpg"))
         .filter(x => !x.includes("_thumb"));
 
@@ -43,30 +43,50 @@ async function start() {
         })
 
         const clonedGalleryFigure = galleryFigure.cloneNode(true);
-
-        const img = clonedGalleryFigure.querySelector("img");
-        const anchor = clonedGalleryFigure.querySelector("a");
-        const caption = clonedGalleryFigure.querySelector("figcaption");
-
-        const captionText = exifData && exifData.image && exifData.image.XPComment
+        
+        
+        const commentText = exifData && exifData.image && exifData.image.XPComment
             ? exifData.image.XPComment.filter(x => x > 0).map(x => String.fromCharCode(x)).join("")
             : "No caption available.";
 
+        
+        const subjectText = exifData && exifData.image && exifData.image.XPSubject
+            ? exifData.image.XPSubject.filter(x => x > 0).map(x => String.fromCharCode(x)).join("")
+            : "No data.";
+
+        const artistText = exifData && exifData.image && exifData.image.Artist
+            ? exifData.image.Artist
+            : null;
+        let artistData;
+        if (artistText) {
+            const artistDataSplit = artistText.split(";");
+            artistData = {
+                name: artistDataSplit[0],
+                href: artistDataSplit[1]
+            };
+        }
+
+        const img = clonedGalleryFigure.querySelector("img");
         const thumbPath = await resizeImage(image, "thumb", 500);
         img.src = thumbPath;
-        img.alt = captionText;
+        img.alt = subjectText;
 
+        const anchor = clonedGalleryFigure.querySelector("a");
         const linkName = path.basename(image, path.extname(image));
-        anchor.href = `/photographs/${linkName}`
-        caption.innerHTML = "";
-        caption.appendChild(galleryDom.window.document.createTextNode(captionText));
+        anchor.href = `/gallery/${linkName}`
+        
 
         imageGallery.appendChild(clonedGalleryFigure);
 
-        const title = exifData && exifData.image && exifData.image.XPTitle
+        const titleText = exifData && exifData.image && exifData.image.XPTitle
             ? exifData.image.XPTitle.filter(x => x > 0).map(x => String.fromCharCode(x)).join("")
             : linkName.split("-").map(x => x.charAt(0).toUpperCase() + x.slice(1)).join(" ")
-        await createImageView(linkName, title, captionText, image);
+
+        const captionElement = clonedGalleryFigure.querySelector("figcaption");
+        captionElement.innerHTML = "";
+        captionElement.appendChild(galleryDom.window.document.createTextNode(titleText));
+
+        await createImageView(linkName, titleText, commentText, subjectText, artistData, image);
     }
 
     const result = galleryDom.serialize();
@@ -82,6 +102,10 @@ async function resizeImage(imagePath, nameDecoration, size) {
 
     const newImagePath = path.join(imageDir, `${imageName}_thumb${imageExt}`)
 
+    if (existsSync(newImagePath)) {
+        return newImagePath;
+    }
+
     await sharp(image)
         .resize(size)
         .toFile(path.join(imageDir, `${imageName}_${nameDecoration}${imageExt}`))
@@ -89,35 +113,49 @@ async function resizeImage(imagePath, nameDecoration, size) {
     return newImagePath;
 }
 
-async function createImageView(linkName, title, description, src) {
-    const imageViewDom = await getDOM(path.join("./", "photographs", "photograph.html"));
+async function createImageView(linkName, title, comment, subject, artist, src) {
+    const imageViewDom = await getDOM(path.join("./", "gallery", "gallery.html"));
     if (!imageViewDom) {
         return;
     }
 
-    const imageViewFigure = imageViewDom.window.document.querySelector(".image-gallery figure");
-    if (!imageViewFigure) {
+    const imageViewFigureElement = imageViewDom.window.document.querySelector(".image-gallery figure");
+    if (!imageViewFigureElement) {
         return;
     }
 
-    const header = imageViewDom.window.document.querySelector("h1");
-    if (!header) {
+    const headerElement = imageViewDom.window.document.querySelector("h1");
+    if (!headerElement) {
         return;
     }
 
-    const img = imageViewFigure.querySelector("img");
-    const caption = imageViewFigure.querySelector("figcaption");
+    const subjectElement = imageViewDom.window.document.querySelector("p");
+    if (!subjectElement) {
+        return;
+    }
 
-    header.innerHTML = "";
-    header.appendChild(imageViewDom.window.document.createTextNode(title))
+    const imgElement = imageViewFigureElement.querySelector("img");
+    const captionElement = imageViewFigureElement.querySelector("figcaption");
 
-    img.src = src;
+    headerElement.innerHTML = "";
+    headerElement.appendChild(imageViewDom.window.document.createTextNode(title))
 
-    caption.innerHTML = "";
-    caption.appendChild(imageViewDom.window.document.createTextNode(description))
+    imgElement.src = src;
+
+    captionElement.innerHTML = "";
+    captionElement.appendChild(imageViewDom.window.document.createTextNode(comment))
+
+    subjectElement.innerHTML = "";
+    subjectElement.appendChild(imageViewDom.window.document.createTextNode(artist ? `${subject} by ` : subject));
+    if (artist) {
+        const artistLinkElement = imageViewDom.window.document.createElement("a");
+        artistLinkElement.href = artist.href;
+        artistLinkElement.innerHTML = artist.name;
+        subjectElement.appendChild(artistLinkElement);
+    }
 
     const result = imageViewDom.serialize();
-    const outputPath = path.join("photographs", `${linkName}.html`)
+    const outputPath = path.join("gallery", `${linkName}.html`)
     return await writeFile(outputPath, result);
 }
 
